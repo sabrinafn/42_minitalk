1270aaa785da479725eac895315ca016d159c372
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-09-27 19:05:27 -0300

    just added comments that might help with implementation

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sabrifer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 18:08:36 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/09/21 19:20:15 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/09/21 20:41:06 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,24 @@
 void	sighandler(int c)
 {
 	printf("signal: %d\n", c);
+}
+
+// create a function to check if the signal was received by the server
+
+void	function_to_convert_char_tobinary()
+{
+	// it will be receiving a char at a time >
+	// it will be converting to binary >
+	// and sending each 0 and 1 as signals using >
+	// SIGUSR1 and SIGUSR2
+	
+}
+
+void	function_to_send_char_to_convert(char *str)
+{
+	// loop through whole array >
+	// send each char to function convert_char_and set one bit at a time >
+	// keep looping untill all chars have been sent
 }
 
 int	main(int ac, char **av)
--- server.c
+++ server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabrifer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 20:41:17 by sabrifer          #+#    #+#             */
+/*   Updated: 2024/09/21 20:44:34 by sabrifer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
 void	sig_handler(int sig)
@@ -5,12 +17,22 @@ void	sig_handler(int sig)
 	printf("sig = %d\n", sig);
 }
 
+// there will be a function which will send back to the client >>
+// like a response, that the signal was received and >>
+// the action was performed
+// maybe after every 8 bits received, transformed into decimal >>
+// then transformed into chars and printed, it will send this signal >>
+// that everything is cool and keep sending more signals to it
+
+// function to convert from each binary digit to a decimal >>
+// which then will be calculated and printed as a char once it its 8 bits
+
 int	main(void)
 {
-	struct sigaction action;
-	action.sa_handler = sig_handler;
 	sigset_t	set;
-
+	struct sigaction	action;
+	
+	action.sa_handler = sig_handler;
 	sigemptyset(&action.sa_mask);
 	action.sa_flags = 0;
 	sigaddset(&set, SIGUSR1);
