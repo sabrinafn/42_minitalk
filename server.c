378f3838eabd98dbffb0d5eb1cbef0f3cc3a5e14
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-09-27 19:05:25 -0300

    just doing some p

--- server.c
+++ server.c
@@ -1,15 +1,42 @@
 #include "minitalk.h"
 
+void	signal_handler(int sig)
+{
+	int	x;
+
+	x = 5;
+	printf("x = %d\n", x);
+	(void)sig;
+}
+
 int	main(void)
 {
 	printf("server\n");
 	pid_t	value = getpid();
-	printf("[PID: %d]\n", value);
+	pid_t	two;
+	printf("[first PID: %d]\n", value);
 
+	signal(SIGUSR1, signal_handler);
+
+	two = fork();
+
+	if (two == 0)
+	{
+		pid_t parent = getppid();
+		kill(parent, SIGUSR1);
+		sleep(1);
+	
+	
+	}
+	else
+	{
+		init stat;
+		wait(&stat);
+	}
 	// SIGUSR1 and SIGUSR2: almost the same thing apart
 	// from the signal number.
 	
 	//kill(value, SIGUSR1);
-	kill(value, SIGUSR2);
+	//kill(value, SIGUSR2);
 	return (0);
 }
